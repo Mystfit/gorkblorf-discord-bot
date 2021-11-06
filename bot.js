410443d572f94ec09dfb41b95d46e9e5caafd12b
Byron Mallett <byronated@gmail.com>
2021-11-06 10:33:08 +0000

    Choosing correct iso code

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const LanguageDetect = require('languagedetect');
 const lngDetector = new LanguageDetect();
 const CountryLanguage = require('country-language');
 const Iso639Type = require('iso-639-language');
-const iso639_2 = Iso639Type["default"].getType(2);
+const iso639_1 = Iso639Type["default"].getType(1);
 
 var markov = require('markov');
 var markov_bot = markov();
@@ -115,7 +115,7 @@ function validate_gorkblorf_message(message)
     message.react('🔴');
 
     violations.forEach(violation => {
-      var lang_code = iso639_2.getCodeByName(violation["language"]); 
+      var lang_code = iso639_1.getCodeByName(violation["language"]); 
       CountryLanguage.getLanguageCountries(lang_code, function (err, countries) {
         if (err) {
           console.log(err);
