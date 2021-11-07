e2f62579738bb7bcb2231c2b342282fb7b76dfea
Byron Mallett <byronated@gmail.com>
2021-11-07 22:57:19 +1300

    Fixing broken training

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const lngDetector = new LanguageDetect();
 const CountryLanguage = require('country-language');
 const Iso639Type = require('iso-639-language');
 const iso639_1 = Iso639Type["default"].getType(1);
-const countryCodeEmoji = require('country-code-emoji');
+const { countryCodeEmoji, emojiCountryCode } = require('country-code-emoji');
 
 var markov = require('markov');
 var markov_bot = markov();
@@ -145,18 +145,16 @@ function validate_gorkblorf_message(message)
     detected_languages = word_language(word);//lngDetector.detect(word);
 
     // We might not match any existing language (gorkblorf!)
-    if(detected_languages.length > 0){
-      if(word.length < 3)
-        return;
+    if(word.length < 3)
+      return;
 
-      // Check the match against the hand-tweaked threshold
-      if(detected_languages[0][1] > language_match_threshold){
-        num_violations += 1;
-        violations.push({"word": word, "language": detected_languages[0][0]});
-        console.log("Violation:", word + ",", "Language:", detected_languages[0][0]+ ",", "Confidence:", detected_languages[0][1]);
-      } else {
-        valid_words.push(word);
-      }
+    // Check the match against the hand-tweaked threshold
+    if(detected_languages[0][1] > language_match_threshold){
+      num_violations += 1;
+      violations.push({"word": word, "language": detected_languages[0][0]});
+      console.log("Violation:", word + ",", "Language:", detected_languages[0][0]+ ",", "Confidence:", detected_languages[0][1]);
+    } else {
+      valid_words.push(word);
     }
   });
 
